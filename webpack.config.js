e9b4ae5e65493a8345385bea8ddf807fd70b535b
Anton <crng@ukr.net>
2022-02-28 17:59:08 +0200

    Changed classes to functions with using hooks in components, remade readme file

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,40 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+import { resolve as _resolve } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-  entry: "./src/App.ts",
-  target: "web",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
-        ],
-      },
-    ],
-    // loaders: [
-    //   { exclude: ["node_modules"], loader: "babel", test: /\.jsx?$/ },
-    //   { loader: "style-loader!css-loader", test: /\.css$/ },
-    //   { loader: "url-loader", test: /\.gif$/ },
-    //   { loader: "file-loader", test: /\.(ttf|eot|svg)$/ },
-    // ],
-  },
-  resolve: {
-    extensions: ["", ".tsx", ".ts", ".js", ".jsx"],
-  },
-  output: {
-    filename: "js/[name].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: true,
-      minify: false,
-    }),
+export const entry = "./src/App.tsx";
+export const target = "web";
+export const module = {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: "ts-loader",
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {
+          loader: "style-loader",
+        },
+        {
+          loader: "css-loader",
+        },
+      ],
+    },
   ],
 };
+export const resolve = {
+  extensions: ["", ".tsx", ".ts", ".js", ".jsx"],
+};
+export const output = {
+  filename: "js/[name].bundle.js",
+  path: _resolve(__dirname, "./dist"),
+  publicPath: "",
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    inject: true,
+    minify: false,
+  }),
+];
